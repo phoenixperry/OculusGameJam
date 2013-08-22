98bf5f37835a3d30307ee6ec7885f256d3ce8071
Phoenix Perry <contact@phoenixperry.com>
2013-08-22 11:53:49 -0300

    adding in a few scripts

---
+++ AberationControls.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AberationControls : MonoBehaviour, IVisTriggerTarget  {
+	Vignetting ca; 
+	float myVal; 
+	void Start(){
+		ca = GetComponent<Vignetting>(); 
+
+	} 
+	// Use this for initialization
+	 public void OnTriggered(float current, float previous, float difference, float adjustedDifference) {
+
+					//ca.chromaticAberration = 150; 
+					 
+
+
+	 }
+
+}
